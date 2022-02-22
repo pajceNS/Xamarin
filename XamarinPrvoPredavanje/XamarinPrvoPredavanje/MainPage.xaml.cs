54732b23bb8cb58efbd1d5e977617119f8f70c70
DESKTOP-H1A1V48\User_10 <nemanjapajcin@gmail.com>
2022-02-22 19:39:30 +0100

    Change new folder

--- XamarinPrvoPredavanje/XamarinPrvoPredavanje/MainPage.xaml.cs
+++ XamarinPrvoPredavanje/XamarinPrvoPredavanje/MainPage.xaml.cs
@@ -5,14 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinPrvoPredavanje.NewFolder;
 
 namespace XamarinPrvoPredavanje
 {
     public partial class MainPage : ContentPage
     {
+        private int counter=0;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            mergedDictionaries.Clear();
+            mergedDictionaries.Add(new Light());
+
+            if (counter%2 == 0)
+            {
+                mergedDictionaries.Add(new Light());
+            }
+            else
+            {
+                mergedDictionaries.Add(new Dark());
+            }
+            counter++;
         }
     }
 }
